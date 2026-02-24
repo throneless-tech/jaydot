eda36a496a72ba8ffd6bc3c1aeb617eea5d0dce7
Rae Gaines <rae@throneless.tech>
2026-02-24 12:51:38 -0500

    working on cms integration, and some style updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://accessible-astro-starter.incluud.dev', // FIXME
+  site: 'https://jaydot.org',
+  srcDir: './src/app/(frontend)/',
   integrations: [
     compress(),
     icon({
       include: {
-        iconDir: "src/assets/icons",
+        iconDir: "./src/app/(frontend)/assets/icons",
       },
     }),
     mdx(),
@@ -33,11 +34,11 @@ export default defineConfig({
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-        '@content': fileURLToPath(new URL('./src/content', import.meta.url)),
-        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/app/(frontend)/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/app/(frontend)/layouts', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/app/(frontend)/assets', import.meta.url)),
+        '@content': fileURLToPath(new URL('./src/app/(frontend)/content', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/app/(frontend)/pages', import.meta.url)),
         '@public': fileURLToPath(new URL('./public', import.meta.url)),
         '@post-images': fileURLToPath(new URL('./public/posts', import.meta.url)),
         '@project-images': fileURLToPath(new URL('./public/projects', import.meta.url)),
---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    reactCompiler: false,
+  },
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.cjs': ['.cts', '.cjs'],
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    }
+
+    return webpackConfig
+  },
+}
+
+export default withPayload(nextConfig, { devBundleServerPackages: false })
