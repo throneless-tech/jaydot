f1732410c49a53c1ca092954d0d80b5e85aba3ae
Rae Gaines <rae@throneless.tech>
2026-02-26 10:25:26 -0500

    restructuring for cms

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'url'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  site: 'https://jaydot.org',
+  srcDir: './src/',
+  integrations: [
+    compress(),
+    icon({
+      include: {
+        iconDir: "./src/assets/icons",
+      },
+    }),
+    mdx(),
+    sitemap()
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => { },
+          },
+        },
+      },
+    },
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@content': fileURLToPath(new URL('./src/content', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@public': fileURLToPath(new URL('./public', import.meta.url)),
+        '@post-images': fileURLToPath(new URL('./public/posts', import.meta.url)),
+        '@project-images': fileURLToPath(new URL('./public/projects', import.meta.url)),
+      },
+    },
+  },
+})
